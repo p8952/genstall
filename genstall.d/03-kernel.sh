9664a098e665ad376d840d6d86ac81e844189be4
Peter Wilmott <p@p8952.info>
2014-01-27 22:33:14 +0000

    remount /boot as rw before installing grub

--- genstall.d/03-kernel.sh
+++ genstall.d/03-kernel.sh
@@ -6,6 +6,7 @@ $CHROOT emerge sys-kernel/gentoo-sources sys-kernel/genkernel sys-boot/grub
 $CHROOT /bin/bash << EOF 
 cd /usr/src/linux 
 genkernel all --makeopts=-j3
+mount -o remount,rw /boot
 grub2-install /dev/sda
 grub2-mkconfig -o /boot/grub/grub.cfg
 EOF
