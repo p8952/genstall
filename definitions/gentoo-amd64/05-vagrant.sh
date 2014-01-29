23f59fe2308472c56aec90de07e76ebf0d12dc27
Peter Wilmott <p@p8952.info>
2014-01-29 08:01:21 +0000

    correct authorized_keys location

--- definitions/gentoo-amd64/05-vagrant.sh
+++ definitions/gentoo-amd64/05-vagrant.sh
@@ -17,7 +17,7 @@ cat > /mnt/gentoo/home/vagrant/.ssh/authorized_keys << EOF
 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ==
 EOF
 $_CHROOT chmod -R 0700 /home/vagrant/.ssh/
-$_CHROOT chmod -R 0600 /vagrant/.ssh/authorized_keys
+$_CHROOT chmod -R 0600 /home/vagrant/.ssh/authorized_keys
 $_CHROOT chown -R vagrant:vagrant /home/vagrant/.ssh/
 
 $_CHROOT emerge app-admin/sudo app-emulation/virtualbox-guest-additions
