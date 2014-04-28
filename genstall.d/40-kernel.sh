d3511f6d9aea6fb6e26ac8890e1c72dda279632b
Peter Wilmott <p@p8952.info>
2014-04-28 18:55:17 +0100

    No idea why it errors without these...

--- genstall.d/40-kernel.sh
+++ genstall.d/40-kernel.sh
@@ -5,7 +5,7 @@ $_CHROOT emerge sys-kernel/gentoo-sources sys-kernel/genkernel sys-boot/grub
 
 $_CHROOT /bin/bash << EOF 
 cd /usr/src/linux 
-genkernel all --makeopts=-j3
+genkernel all --makeopts=-j3 --no-compress-initrd --no-zfs
 mount -o remount,rw /boot
 grub2-install /dev/sda
 grub2-mkconfig -o /boot/grub/grub.cfg
