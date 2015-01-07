17703bf5cb97f9493fbb7eec0bce12adf3b1611f
Peter Wilmott <p@p8952.info>
2015-01-07 15:29:33 +0000

    Tidy up after updating world

--- genstall.d/50-software.sh
+++ genstall.d/50-software.sh
@@ -9,4 +9,9 @@ for D in $DAEMONS; do
 	$_CHROOT rc-update add $D default
 done
 
+_EMERGE app-portage/gentoolkit
 $_CHROOT emerge -uND @world
+$_CHROOT emerge --depclean
+$_CHROOT revdep-rebuild
+$_CHROOT rm -rf /var/tmp/portage/*
+$_CHROOT rm -rf /usr/portage/distfiles/*
